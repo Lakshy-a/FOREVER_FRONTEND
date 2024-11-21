6909b4671df66987981fc02e509ce1848b26d933
Lakshya_Gupta <lakshyagupta8786@gmail.com>
2024-11-21 18:23:07 +0530

    Made the list items responsive

--- src/Components/adminPanelComponents/ListItems.jsx
+++ src/Components/adminPanelComponents/ListItems.jsx
@@ -49,57 +49,104 @@ const ListItems = () => {
   }, []);
 
   return (
-    <div className="px-4 xs:pl-16 mt-8 mb-16">
-      <div>All Products List</div>
-      <div className="border mt-4 flex pl-4 bg-gray-100 border-gray-300">
-        <div className="w-[130px] font-semibold">Image</div>
-        <div className="w-[260px] font-semibold">Name</div>
-        <div className="w-[130px] font-semibold">Category</div>
-        <div className="w-[130px] font-semibold">Price</div>
-        <div className="w-[130px] font-semibold">Status</div>
-        <div className="w-[130px] font-semibold">Action</div>
-      </div>
-      <div className="mt-4">
-        {allProducts.map((product) => (
-          <div
-            className="flex mt-3 border border-gray-400 rounded-md overflow-hidden"
-            key={product._id}
-          >
-            <div className="w-[130px]">
-              <img
-                src={product.images[0]}
-                alt={product.productName}
-                className="w-12"
-              />
-            </div>
-            <div className="w-[300px] flex items-center text-sm font-semibold text-gray-500">
-              {product.productName}
-            </div>
-            <div className="w-[130px] flex items-center text-sm font-semibold text-gray-500">
-              {product.productCategory}
-            </div>
-            <div className="w-[130px] text-sm font-semibold text-gray-500 flex items-center">
-              ${product.productDiscountedPrice}
-            </div>
+    <>
+      {/* it will work only for widths more than 468 */}
+      <div className="hidden md:block px-4 xs:pl-16 mt-8 mb-16">
+        <div>All Products List</div>
+        <div className="border mt-4 flex pl-4 bg-gray-100 border-gray-300">
+          <div className="w-[130px] font-semibold">Image</div>
+          <div className="w-[260px] font-semibold">Name</div>
+          <div className="w-[130px] font-semibold">Category</div>
+          <div className="w-[130px] font-semibold">Price</div>
+          <div className="w-[130px] font-semibold">Status</div>
+          <div className="w-[130px] font-semibold">Action</div>
+        </div>
+        <div className="mt-4">
+          {allProducts.map((product) => (
             <div
-              className={`w-[130px] text-sm font-semibold ${
-                product.isDeleted ? "text-red-600" : "text-green-600"
-              } flex items-center`}
+              className="flex mt-3 border border-gray-400 rounded-md overflow-hidden"
+              key={product._id}
             >
-              {product.isDeleted ? "Draft" : "Active"}
+              <div className="w-[130px]">
+                <img
+                  src={product.images[0]}
+                  alt={product.productName}
+                  className="w-12"
+                />
+              </div>
+              <div className="w-[300px] flex items-center text-sm font-semibold text-gray-500">
+                {product.productName}
+              </div>
+              <div className="w-[130px] flex items-center text-sm font-semibold text-gray-500">
+                {product.productCategory}
+              </div>
+              <div className="w-[130px] text-sm font-semibold text-gray-500 flex items-center">
+                ${product.productDiscountedPrice}
+              </div>
+              <div
+                className={`w-[130px] text-sm font-semibold ${
+                  product.isDeleted ? "text-red-600" : "text-green-600"
+                } flex items-center`}
+              >
+                {product.isDeleted ? "Draft" : "Active"}
+              </div>
+              <div
+                className={`w-[130px] flex items-center justify-center text-xl font-semibold text-gray-500 cursor-pointer ${
+                  product.isDeleted ? "text-green-600" : "text-red-600"
+                }`}
+                onClick={() => handleRemove(product)}
+              >
+                {product.isDeleted ? <MdRestore /> : <MdDelete />}
+              </div>
             </div>
-            <div
-              className={`w-[130px] flex items-center justify-center text-xl font-semibold text-gray-500 cursor-pointer ${
-                product.isDeleted ? "text-green-600" : "text-red-600"
-              }`}
-              onClick={() => handleRemove(product)}
-            >
-              {product.isDeleted ? <MdRestore /> : <MdDelete />}
-            </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
+
+      {/* it will word for width less than 768px */}
+      <div className="md:hidden grid grid-cols-2 gap-4 pl-8 ">
+        {allProducts.map((product, index) => {
+          return (
+            <div
+              className="mt-4 cursor-pointer bg-gray-200 overflow-hidden rounded-xl"
+              key={index}
+            >
+              <div className="rounded-t-xl">
+                <img
+                  className="hover:scale-110 duration-200 h-[150px] w-[190px] object-fill"
+                  src={product.images[0]}
+                  alt={product.productName}
+                />
+              </div>
+              <h1 className="mt-2 text-xs font-normal px-2">
+                {product.productName}
+              </h1>
+              <div className="mt-2 text-xs px-2 flex ">
+                <span className="font-semibold">Price :</span> $
+                {product.productPrice}
+              </div>
+              <div className="flex justify-between items-center pr-2 my-2">
+                <div
+                  className={`w-[130px] text-sm font-semibold px-2  ${
+                    product.isDeleted ? "text-red-600" : "text-green-600"
+                  } flex items-center`}
+                >
+                  {product.isDeleted ? "Draft" : "Active"}
+                </div>
+                <div
+                  className={`w-fit xs:w-[130px] text-xl font-semibold text-gray-500 cursor-pointer ${
+                    product.isDeleted ? "text-green-600" : "text-red-600"
+                  }`}
+                  onClick={() => handleRemove(product)}
+                >
+                  {product.isDeleted ? <MdRestore /> : <MdDelete />}
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 };
 
--- src/Components/adminPanelComponents/Users.jsx
+++ src/Components/adminPanelComponents/Users.jsx
@@ -1,7 +1,12 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { FaPhone } from "react-icons/fa6";
+import { MdEmail } from "react-icons/md";
+import { PiDotsThreeOutlineFill } from "react-icons/pi";
 
 const Users = () => {
+  const [allUsers, setAllUsers] = useState([]);
+
   useEffect(() => {
     axios
       .get("http://localhost:3001/api/admin/getAllUsers", {
@@ -9,13 +14,47 @@ const Users = () => {
       })
       .then((response) => {
         console.log(response.data); // Accessing the data from response
+        setAllUsers(response.data.data);
       })
       .catch((error) => {
         console.error("Error fetching users:", error);
       });
   }, []);
 
-  return <div>Users</div>;
+  return (
+    <div className="px-4 xs:pl-8 mt-8 mb-16">
+      <div className="text-xl font-semibold">Users List</div>
+      {/* Grid Layout */}
+      <div className="mt-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 md:gap-4 lg:grid-cols-3 gap-4 xs:gap-16 xl:grid-cols-4 xl:gap-24">
+        {allUsers.map((user, index) => (
+          <div
+            key={user._id}
+            className="w-[220px] h-36 rounded-xl bg-white flex flex-col items-center pt-2 gap-1 shadow-lg border border-black relative"
+          >
+            <div className="w-10 h-10 rounded-full bg-black text-white flex justify-center items-center text-2xl font-semibold">
+              {user.name[0].toUpperCase()}
+            </div>
+            <div className="text-lg font-medium">{user.name}</div>
+            <div className="px-8 flex flex-col w-full items-center">
+              <div className="flex items-center gap-2">
+                <span>
+                  <FaPhone className="text-sm" />
+                </span>
+                <span>{user.phone}</span>
+              </div>
+              <div className="flex items-center gap-2">
+                <span>
+                  <MdEmail className="text-sm" />
+                </span>
+                <span>{user.email}</span>
+              </div>
+            </div>
+            <div className="absolute right-4 cursor-pointer"><PiDotsThreeOutlineFill /></div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Users;
